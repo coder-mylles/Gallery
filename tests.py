782bc1cb2c67ebdfe25e19c52a6f2b822d2c26a8
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-06 02:08:37 +0300

    add tests to the app

---
+++ tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import Tag, Photos, Location, Category
+import datetime as dt
+
+
+# Create your tests here.	# Create your tests here.
+class TagTestCase(TestCase):
+    def setUp(self):
+        self.life = Tag(name='life')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.life, Tag)) 
