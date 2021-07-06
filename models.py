6dbf1fab76ba52222abf7928c1e5b28318be0953
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-06 09:19:07 +0300

    add category models

--- models.py
+++ models.py
@@ -5,21 +5,16 @@ class Location(models.Model):
     name = models.CharField(max_length=30)
     def __str__(self):
         return self.name
-
     def save_location(self):
         self.save()
-
     def delete_location(self):
         self.delete()
-
     @classmethod
     def update_location(cls,id,name):
         cls.objects.filter(id = id).update(name = name)
-
     @classmethod
     def display_all_locations(cls):
         return cls.objects.all()
-
 class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
@@ -28,6 +23,17 @@ class Tag(models.Model):
     name = models.CharField(max_length=30)
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id = id).update(name = name)
+
 class Photos(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
